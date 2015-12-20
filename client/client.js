45c6fbe8fc9643489767d60c0b7af5f3c583d204
Evan <evangulyas@outlook.com>
2015-12-19 18:40:08 -0800

    Fix localhost

--- client/client.js
+++ client/client.js
@@ -245,7 +245,7 @@ function clientCtrl($scope, roomsFactory, messagesFactory, usersFactory, sendFil
       'force new connection': true
     };
 
-    socket = io('http://localhost:8080', options);
+    socket = io(window.location.href);
 
     socket.on('connect', function () {
 
